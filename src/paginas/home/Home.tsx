ef2d8ae6affffcf24c6addd2275041b92f4cca3d
Fernando Delgado Azevedo <fernandodelgadoazevedo@gmail.com>
2023-02-27 16:49:26 -0300

    Atualizãcao

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="http://github.com/fernandodelgadoazevedo.png" alt="Imagem do GitHub" className="img"/>
+        </>
+    );
+}
+
+export default Home;
