19b1526292e8ad6fa3b508dc214153f86c22fb1a
Fernando Delgado Azevedo <fernandodelgadoazevedo@gmail.com>
2023-03-11 14:50:59 -0300

    git commit -m "Atualização 3.0"

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import Home from './paginas/home/Home';
-import Navbar from './componentes/estaticos/navbar/Navbar';
-import Footer from './componentes/estaticos/footer/Footer';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/estaticos/navbar/Navbar";
+import Footer from "./components/estaticos/footer/Footer";
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
+import Home from "./paginas/home/Home";
+import Login from "./paginas/login/Login";
+import "./App.css";
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
 
 function App() {
-return (
-    <>
-        <Navbar />
-            <Home />
-        <Footer />
-    </>
-);
+  return (
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: "100vh" }}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+          <Route path="/temas" element={<ListaTema />} />
+          <Route path="/posts" element={<ListaPostagem />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/estaticos/footer/Footer.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import GitHubIcon from "@material-ui/icons/GitHub";
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import {Typography,Grid } from '@material-ui/core';
+import {Box} from '@mui/material';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+                <Grid alignItems="center" item xs={12}>
+                    <Box className='box1'>
+                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                            
+                        </Box>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <a href="https://www.github.com/fernandodelgadoazevedo" target="_blank" rel="noopener noreferrer">
+                                <GitHubIcon className='redes' />
+                            </a>
+                            <a href="https://api.whatsapp.com/send/?phone=5511983078800&text=Ol%C3%A1%21+Vim+Direto+do+Blog+Azevedo&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer">
+                                <WhatsAppIcon className='redes' />
+                            </a>
+                            <a href="https://www.linkedin.com/in/fernandodazevedo/" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon className='redes' />
+                            </a>
+                        </Box>
+                    </Box>
+                    <Box className='box2'>
+                        <Box paddingTop={1}>
+                            <Typography variant="subtitle2" align="center" gutterBottom >© 2023 Copyright:</Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </>
+    )
+}
+export default Footer;
